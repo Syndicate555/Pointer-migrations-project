be8dbaee04d9809e1ec49894cf465227387ee2fc
Saffat Aziz <saffataziz@Saffats-MacBook-Pro.local>
2021-07-09 14:44:35 -0400

    Added metafields code

--- source.js
+++ source.js
@@ -16,7 +16,32 @@ const shopify = new Shopify({
 
 const doStuff = async () => {
   const shop = await shopify.shop.get();
+  console.log(shop);
 };
+
+const testFields = [
+  {
+    key: "warehouse_1",
+    value: 123,
+    value_type: "integer",
+    namespace: "inventory",
+    owner_resource: "product",
+  },
+  {
+    key: "warehouse_2",
+    value: "two!",
+    value_type: "string",
+    namespace: "inventory",
+    owner_resource: "product",
+  },
+  {
+    key: "warehouse_3",
+    value: "three!",
+    value_type: "string",
+    namespace: "inventory",
+    owner_resource: "product",
+  },
+];
 
 const getProducts = async () => {
   let params = { limit: 250 };
@@ -29,6 +54,23 @@ const getProducts = async () => {
     params = products.nextPageParameters;
   } while (params !== undefined);
   return results;
+};
+const addMetafields = async () => {
+  const promises = [];
+  const products = await getProducts();
+  products.forEach((product) => {
+    return testFields.forEach((field) => {
+      return promises.push(
+        shopify.metafield.create({
+          ...field,
+          owner_id: product.id,
+        })
+      );
+    });
+  });
+  let counting = promises.length;
+  shopify.on("callLimits", () => console.log(--counting));
+  return Promise.all(promises);
 };
 
 const addProducts = async () => {
@@ -72,7 +114,7 @@ const deleteMetafields = async () => {
   try {
     await db.sequelize.sync();
     // await addProducts();
-    await deleteMetafields();
+    await addMetafields();
   } catch (error) {
     console.log(error);
   }
